283a3197d6ce08bd64db3620b9b0879ba56908dd
izanagi389 <izanagiokami389@gmail.com>
2022-03-12 16:56:14 +0900

    apiの仕様を変更

--- server/api/microcms.ts
+++ server/api/microcms.ts
@@ -1,8 +1,9 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import axios from 'axios'
 import config from '#config'
+import { useBody, useCookies, useQuery } from 'h3'
 
-const ENDPOINT = `https://${config.MICRO_CMS_SERVICE_DOMAIN}.microcms.io/api/v1/blog`
+let url = `https://${config.MICRO_CMS_SERVICE_DOMAIN}.microcms.io/api/v1/blog`
 const API_HEAD = {
   headers: {
     "X-MICROCMS-API-KEY": config.MICRO_CMS_API_KEY,
@@ -16,15 +17,35 @@ export default async (req: IncomingMessage, res: ServerResponse) => {
     res.end()
   }
 
+
+  const post_id = !!useQuery(req).id ? "/" + useQuery(req).id : "";
+  const post_offset = !!useQuery(req).offset ? useQuery(req).offset : "";
+  const post_limit = !!useQuery(req).limit ? useQuery(req).limit : "10";
+  const post_order = !!useQuery(req).order ? useQuery(req).order : "";
+  const post_fields = !!useQuery(req).fields ? useQuery(req).fields : "";
+  const post_filters = useQuery(req).filters ? useQuery(req).filters : "";
+
+  console.log(!!useQuery(req).limit)
+
+
+  const parameters = `${post_id}?fields=${post_fields}&filters=${post_filters}&limit=${post_limit}&offset=${post_offset}&orders=${post_order}`;
+  const ENDPOINT = url + parameters;
+
+
   let data: Array<any>
   await axios.get(
     `${ENDPOINT}`,
     API_HEAD
   ).then(res => {
-    data = res.data.contents;
-  });
+    if (!!post_id) {
+      data = res.data;
+    } else {
+      data = res.data;
+    }
 
+  });
   const json = JSON.stringify(data)
+
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
   res.end(json)
