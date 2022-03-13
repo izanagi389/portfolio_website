a1773d4a8f927c7da074f4ac6f345514f65c1036
izanagi389 <izanagiokami389@gmail.com>
2022-03-14 01:08:12 +0900

    リダイレクト設定を追加

---
+++ middleware/route.global.ts
@@ -0,0 +1,5 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+    if (to.path === '/blog' || to.path === '/blog/' || to.path === '/blog/pages' || to.path === '/blog/pages/' ) {
+        return navigateTo('/blog/pages/1')
+    }
+})
