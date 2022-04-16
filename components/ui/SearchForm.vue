0fc157ed546e1a2f519011edb4ad82e1fa0cd15a
izanagi389 <izanagiokami389@gmail.com>
2022-04-16 20:01:43 +0900

    検索フォームをコンポーネント化

---
+++ components/ui/SearchForm.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="search_box">
+        <form method="get" action="/blog/search">
+            <input name="word" type="text" required v-model="word" :placeholder="placeholder" />
+            <v-btn icon="mdi-magnify" color="light-blue" rel="noopener noreferrer" size="small" type="submit">
+            </v-btn>
+        </form>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+const props = defineProps({
+    placeholder: String
+})
+
+const placeholder = props.placeholder;
+
+const word = ref('');
+</script>
+
+<style lang="scss" scoped>
+.search_box {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 10px 5px;
+
+    form {
+        input {
+            width: 85%;
+            height: 40px;
+            border-style: double !important;
+
+            &:focus {
+                outline: 0;
+            }
+        }
+
+        button {
+            top: -3px;
+        }
+    }
+}
+</style>
