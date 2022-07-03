1b03448500306781c7250e95b8524ca6e32b1aeb
izanagi389 <izanagiokami389@gmail.com>
2022-07-03 17:40:20 +0900

    メソッドの整理

--- error.vue
+++ error.vue
@@ -1,7 +1,7 @@
 <template>
     <v-app id="top">
         <div id="error_page">
-            <TheHeader :tagsList="tagsList" />
+            <TheHeader />
             <v-main>
                 <div id="error_box">
                     <h1>404</h1>
@@ -12,21 +12,14 @@
                     </div>
                 </div>
             </v-main>
+            <TheFooter />
         </div>
     </v-app>
 </template>
 
 
 <script lang="ts" setup>
-let { data } = await useFetch("/api/microcms", {
-    params: {
-        limit: 1000,
-    },
-});
 
-let tags = "";
-data.value["contents"].forEach((element) => tags += element.tags)
-const tagsList = Array.from(new Set(tags.split(","))).join(",").split(",");
 </script>
 
 
