9fd3f06ca7244fcdd5458f3119ff904c8fe47cc8
izanagi389 <izanagiokami389@gmail.com>
2022-10-25 06:45:18 +0900

    metaタグのタイトル等の調整

--- pages/contact.vue
+++ pages/contact.vue
@@ -7,4 +7,20 @@
 </template>
 
 <script lang="ts" setup>
+const route = useRoute()
+const config = useRuntimeConfig()
+const title = "お問い合わせ";
+const description = "お問い合わせページになります。"
+const url = config.HOMEPAGE_ROOT_URL + route.fullPath;
+
+useHead({
+    title: title,
+    meta: [
+        { property: 'og:title', hid: 'og:title', content: title },
+        { name: 'description', hid: 'description', content: description },
+        { property: 'og:description', hid: 'og:description', content: description },
+        { property: 'og:url', hid: 'og:url', content: url },
+        { name: 'robots', content: 'noindex,nofollow' }
+    ],
+})
 </script>
