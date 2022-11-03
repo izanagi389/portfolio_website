ed666d7fcc830b5098ff0b49cd541b539c87ed4c
izanagi389 <izanagiokami389@gmail.com>
2022-11-03 21:43:04 +0900

    アドセンス用コンポーネント（調整中）

--- components/blog/ui/adsense/Landscape.vue
+++ components/blog/ui/adsense/Landscape.vue
@@ -1,17 +1,29 @@
 <template>
+  <client-only>
     <ins class="adsbygoogle" style="display:block" :data-ad-client="adsenseID" data-ad-slot="3707681924"
-        data-ad-format="auto" data-full-width-responsive="true"></ins>
+      data-ad-format="auto" data-full-width-responsive="true"></ins>
+  </client-only>
 </template>
 
 <script setup>
 
-const config = useRuntimeConfig();
+const props = defineProps({
+  adsenseID: String
+})
 
-const adsenseID = ref(config.ADSENCE_CLIENT_ID);
+const adsenseID = ref(props.adsenseID)
 
 onMounted(() => {
   nextTick(() => {
-    (adsbygoogle = window.adsbygoogle || []).push({})
+    (adsbygoogle = window.adsbygoogle || []).push({});
   })
-})
+}) 
 </script>
+
+
+<style scoped>
+/* ➍ 広告未配信なら広告セクションを非表示にする */
+ins.adsbygoogle[data-ad-status="unfilled"] {
+  display: none !important;
+}
+</style>
