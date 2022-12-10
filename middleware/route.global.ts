ba0a18921079d5ea6ca4b71c764f42f989bc9627
izanagi389 <izanagiokami389@gmail.com>
2022-12-10 16:21:28 +0900

    nuxt.config.tsにrouteRules追加に伴い削除

--- middleware/route.global.ts
+++
@@ -1,5 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-    if (to.path === '/blog' || to.path === '/blog/' || to.path === '/blog/pages' || to.path === '/blog/pages/') {
-        return navigateTo('/blog/pages/1')
-    }
-})
