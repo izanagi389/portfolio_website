6331462a3e545875bd6d58c272f624d76af45865
izanagi389 <izanagiokami389@gmail.com>
2023-05-13 19:32:22 +0900

    text-centerクラスを削除

--- components/top/Information.vue
+++ components/top/Information.vue
@@ -27,7 +27,7 @@
                 </v-card>
             </v-col>
             <v-col>
-                <v-card class="mx-auto text-center" max-width="250" height="500">
+                <v-card class="mx-auto" max-width="250" height="500">
                     <UiSearchForm :placeholder="information.search.placeholder"
                         :suggest-num="information.search.suggest.num" :input-width="'170px'" />
                     <TopUiTags :tag-list="tagList" />
