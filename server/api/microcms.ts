2fca434f101e3f969663f40d08f278fce557d171
izanagi389 <izanagiokami389@gmail.com>
2024-12-22 03:19:23 +0900

    記事詳細も取得可能なように調整

--- server/api/microcms.ts
+++ server/api/microcms.ts
@@ -5,20 +5,34 @@
 // }
 export default defineEventHandler(async (event) => {
 
-    const query = getQuery(event)
+  const query = getQuery(event)
 
-    const limit = query.limit != undefined ? query.limit : 10;
-    const offset = query.offset != undefined ? query.offset : 0;
+  const limit = query.limit != undefined ? query.limit : 10;
+  const offset = query.offset != undefined ? query.offset : 0;
 
-    const data = await $fetch('https://izanagiblog.microcms.io/api/v1/blog', {
-        headers: {
-          "X-MICROCMS-API-KEY": process.env?.X_MICROCMS_API_KEY
-        },
-        params: {
-            "limit": limit,
-            "offset": offset
-        }
-      })
-    return data
+  const post_id = query.post_id;
+
+  let data;
+
+  if (post_id != undefined) {
+    data = await $fetch(`https://izanagiblog.microcms.io/api/v1/blog/${post_id}`, {
+      headers: {
+        "X-MICROCMS-API-KEY": process.env?.X_MICROCMS_API_KEY
+      }
+    })
+  } else {
+    data = await $fetch('https://izanagiblog.microcms.io/api/v1/blog', {
+      headers: {
+        "X-MICROCMS-API-KEY": process.env?.X_MICROCMS_API_KEY
+      },
+      params: {
+        "limit": limit,
+        "offset": offset
+      }
+    })
+  }
+
+
+  return data
 
 })
