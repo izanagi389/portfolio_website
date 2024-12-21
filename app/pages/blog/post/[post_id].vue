af24165d3a0f4d2585d0f69b148d21d100a3ec07
izanagi389 <izanagiokami389@gmail.com>
2024-12-22 03:21:18 +0900

    shiki導入

--- app/pages/blog/post/[post_id].vue
+++ app/pages/blog/post/[post_id].vue
@@ -1,10 +1,91 @@
 <template>
-    <main class="pt-20">
-        <div class="">記事ページ</div>
-    </main>
+  <main class="pt-20">
+    <section>
+      <div
+        class="w-full text-center text-3xl pb-3 text-white drop-shadow-[0_0px_3px_rgba(0,0,0,1)]"
+      >
+        記事タイトル
+      </div>
+
+      <section>
+        <template v-for="c in contents" :key="c.id">
+          <div v-html="c.content"></div>
+          <Shiki
+            v-if="c.viewHtml.includes('blockquote') == false"
+            :code="c.viewHtml"
+            theme="github-dark-dimmed"
+            :lang="c.lang"
+          />
+          <div v-else v-html="c.viewHtml" />
+        </template>
+      </section>
+    </section>
+  </main>
 </template>
 
-<script setup>
-const route = useRoute()
-console.log(route.params.post_id)
+<script setup lang="ts">
+const route = useRoute();
+const post_id = route.params.post_id;
+
+// 初期表示の記事データを取得
+let { data } = await useAsyncData("mountains", () =>
+  $fetch(`/api/microcms?post_id=${post_id}`)
+);
+let contents = data.value.blogContent;
+
+const removetags = (element: string) => {
+  let e;
+
+  if (typeof element == "string") {
+    e = element.replace(/&lt;/g, "<").replace(/&gt;/g, ">");
+    e = e.replace(/<.*pre.*>/g, "");
+    e = e.replace(/<.*code.*>/g, "");
+    e = e.trim();
+  } else {
+    e = "";
+  }
+
+  return e;
+};
+
+const codeLang = (element: string) => {
+  let lang;
+
+  if (typeof element == "string") {
+    if (element.search(/lang-.*>/g) !== -1) {
+      lang = element.match(/lang-.*>/g);
+      if (lang !== null) {
+        lang = lang[0].replace("lang-", "").replace('">', "");
+      }
+    } else if (element.search(/language-.*>/g) !== -1) {
+      lang = element.match(/language-.*>/g);
+      if (lang !== null) {
+        lang = lang[0].replace("language-", "").replace('">', "");
+      }
+    } else {
+      lang = "";
+    }
+  } else {
+    lang = "";
+  }
+
+  if (lang == "markup" || lang == "nginx") {
+    lang = "markdown";
+  } else if (lang == "bash") {
+    lang = "shellscript";
+  }
+
+  return lang;
+};
+
+contents.forEach((element) => {
+  element.lang = codeLang(element.html);
+  element.viewHtml = removetags(element.html);
+});
 </script>
+
+<style>
+.shiki {
+  display: block;
+}
+</style>
