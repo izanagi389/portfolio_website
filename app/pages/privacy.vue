6b40be39b61cbdcf8e85a2be21f296471ad5acb6
izanagi389 <izanagiokami389@gmail.com>
2025-01-03 16:21:10 +0900

    コードの整理

--- app/pages/privacy.vue
+++ app/pages/privacy.vue
@@ -5,39 +5,45 @@
                 Privacy Policy
             </div>
             <div class="bg-white p-10 m-10">
-
-                <!-- 参考 https://xs832297.xsrv.jp/blog-privacy-policy/#s1  -->
-                <div>
-                    Izanagi's-portfolio-site（以下、「当サイト」とします。）は、訪問者のプライバシーを尊重し、個人情報の保護に努めています。本プライバシーポリシーは、当サイトがどのように情報を収集、使用、管理するかを説明しています。
-                </div>
-
-                <h2>個人情報の収集と利用</h2>
-                <div>
-                    当サイトでは、コメント投稿やお問い合わせ時に、名前（ハンドルネーム）、メールアドレス等の個人情報をお尋ねすることがあります。これらの情報は、お問い合わせへの回答や必要な情報を電子メールでご連絡する場合にのみ使用し、これらの目的以外では使用いたしません。
-                </div>
-
-                <h2>クッキー（Cookie）について</h2>
-                <div>
-                    当サイトは、Googleアドセンスによる広告配信や訪問者のサイト利用状況の把握のためにクッキー（Cookie）を使用することがあります。クッキーは、訪問者のブラウザを通じて情報を収集しますが、個人を特定するものではありません。
-                    Cookieを無効にする方法は「<a
-                        href="https://policies.google.com/technologies/ads?gl=jp">広告ーポリシーと規約ーGoogle</a>」をご確認ください。</div>
-
-                <h2>アクセス解析ツールについて</h2>
-                <div>当サイトでは、Googleアナリティクスを使用して、サイトの利用状況を分析しています。Googleアナリティクスもクッキーを使用してデータを収集しますが、個人を特定する情報は含まれません。
-                    なお、この規約に関しては、「<a
-                        href="https://marketingplatform.google.com/about/analytics/terms/jp/">Googleアナリティクス利用規約</a>」または「<a
-                        href="https://policies.google.com/technologies/partner-sites">Googleポリシーと規約」をご覧ください。</a></div>
-
-                <h2>情報の共有と開示</h2>
-                <div>当サイトは、法的要求がある場合や、当サイトの権利、財産、安全の保護のために必要と判断される場合を除き、個人情報を第三者に開示または共有しません。</div>
-
-                <h2>プライバシーポリシーの変更について</h2>
-                <div>当サイトは、必要に応じて本プライバシーポリシーを変更することがあります。変更後のプライバシーポリシーは、当サイト上で公開することで効力を発揮します。</div>
+                <template v-for="p in privacy">
+                    <h2 v-if="p.title.length > 0">{{ p.title }}</h2>
+                    <div v-if="p.content.length > 0">{{ p.content }}</div>
+                </template>
             </div>
-
         </section>
     </main>
 </template>
+
+<script setup lamg="ts">
+
+// 参考 https://xs832297.xsrv.jp/blog-privacy-policy/#s1
+const privacy = [
+    {
+        title: "",
+        content: "Izanagi's-portfolio-site（以下、「当サイト」とします。）は、訪問者のプライバシーを尊重し、個人情報の保護に努めています。本プライバシーポリシーは、当サイトがどのように情報を収集、使用、管理するかを説明しています。"
+    },
+    {
+        title: "個人情報の収集と利用",
+        content: "当サイトでは、コメント投稿やお問い合わせ時に、名前（ハンドルネーム）、メールアドレス等の個人情報をお尋ねすることがあります。これらの情報は、お問い合わせへの回答や必要な情報を電子メールでご連絡する場合にのみ使用し、これらの目的以外では使用いたしません。"
+    },
+    {
+        title: "クッキー（Cookie）について",
+        content: "当サイトは、Googleアドセンスによる広告配信や訪問者のサイト利用状況の把握のためにクッキー（Cookie）を使用することがあります。クッキーは、訪問者のブラウザを通じて情報を収集しますが、個人を特定するものではありません。Cookieを無効にする方法は「<a href='https://policies.google.com/technologies/ads?gl=jp'>広告ーポリシーと規約ーGoogle</a>」をご確認ください。"
+    },
+    {
+        title: "アクセス解析ツールについて",
+        content: "当サイトでは、Googleアナリティクスを使用して、サイトの利用状況を分析しています。Googleアナリティクスもクッキーを使用してデータを収集しますが、個人を特定する情報は含まれません。なお、この規約に関しては、「<a href='https://marketingplatform.google.com/about/analytics/terms/jp/''>Googleアナリティクス利用規約</a>」または「<a href='https://policies.google.com/technologies/partner-sites'>Googleポリシーと規約」をご覧ください。</a>"
+    },
+    {
+        title: "情報の共有と開示",
+        content: "当サイトは、法的要求がある場合や、当サイトの権利、財産、安全の保護のために必要と判断される場合を除き、個人情報を第三者に開示または共有しません。"
+    },
+    {
+        title: "プライバシーポリシーの変更について",
+        content: "当サイトは、必要に応じて本プライバシーポリシーを変更することがあります。変更後のプライバシーポリシーは、当サイト上で公開することで効力を発揮します。"
+    },
+]
+</script>
 
 <style>
 h2 {
