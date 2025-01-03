610fedcfed4201a85e3f7a199235b03b5106242f
izanagi389 <izanagiokami389@gmail.com>
2025-01-03 17:02:04 +0900

    cssを微調整

--- app/components/parts/Title.vue
+++ app/components/parts/Title.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full text-center text-3xl pb-3 mt-10 text-white drop-shadow-[0_0px_3px_rgba(0,0,0,1)]">
+    <div class="w-full text-center text-3xl pb-3 mt-10 mb-10 text-white drop-shadow-[0_0px_3px_rgba(0,0,0,1)]">
         {{ title }}
     </div>
 </template>
