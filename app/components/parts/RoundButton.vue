94d8fa37db1200cb378d7a3f4b48ac97aee73468
izanagi389 <izanagiokami389@gmail.com>
2025-01-18 16:46:06 +0900

    共通コンポーネント化

--- app/components/parts/RoundButton.vue
+++ app/components/parts/RoundButton.vue
@@ -2,21 +2,22 @@
   <button
     class="w-72 p-4 text-sky-500 hover:text-white bg-white hover:bg-sky-500 shadow-lg shadow-blue-500/50 hover:shadow-inherit rounded-full duration-300"
     v-if="props.more_btn_show"
-    @click="moreContents"
+    @click="clickMethod"
   >
-    More
+    {{ props.text }}
   </button>
 </template>
 
 <script setup lang="ts">
 interface Props {
-    more_btn_show: boolean
+    more_btn_show: boolean,
+    text: string
 }
 const props = defineProps<Props>()
 const emits = defineEmits<{
-    (e: 'more-contents'): void
+    (e: 'click-method'): void
 }>()
-const moreContents = () => {
-    emits('more-contents')
+const clickMethod = () => {
+    emits('click-method')
 }
 </script>
