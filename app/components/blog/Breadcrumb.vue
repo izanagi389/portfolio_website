d6a880735d6a510c6761990a9154012e235228ca
izanagi389 <izanagiokami389@gmail.com>
2025-01-18 16:45:30 +0900

    パンくずの階層が増えた場合でも対応可能なように修正

--- app/components/blog/Breadcrumb.vue
+++ app/components/blog/Breadcrumb.vue
@@ -5,7 +5,8 @@
         <a href="/blog/" class="hover:opacity-50 ml-4 mr-4" >Articles</a>
         <template v-for="breadcrumb in props.breadcrumb">
             <span>/</span>
-            <a href="/blog/" class="hover:opacity-50 ml-4 mr-4" >{{ breadcrumb }}</a>
+            <a v-if="props.breadcrumb.lastIndexOf(breadcrumb) !== props.breadcrumb.length -1" href="/blog/" class="hover:opacity-50 ml-4 mr-4" >{{ breadcrumb }}</a>
+            <span class="ml-4 mr-4" >{{ breadcrumb }}</span>
         </template>
                 
     </div>
